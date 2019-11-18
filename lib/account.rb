fd4fffd4205411e63585e877d80e6afa8b8efc10
Heli Sivunen <heli@mooncat.tv>
2019-11-18 16:44:58 +0000

    Print statement passes deposit, fails withdrawal.

--- lib/account.rb
+++ lib/account.rb
@@ -1,27 +1,34 @@
 # frozen_string_literal: true
 
 class Account
-  attr_accessor :balance
+  attr_accessor :balance, :show_balance, :deposit, :withdrawal
 
   def initialize
     @balance = 0
+    @show_balance = show_balance
   end
 
   def deposit(money)
-    self.balance += money
+    @deposit_amount = self.balance += money
     # Time.now.strftime('%d/%m/%Y')
   end
 
   def withdraw(money)
-    self.balance -= money
-    # Time.now.strftime('%d/%m/%Y')
+    if balance <= 0
+      p '0'
+    else
+      @withdrawal_amount = self.balance -= money
+      # Time.now.strftime('%d/%m/%Y')
+    end
   end
 
   def show_balance
-    '%.2f' % @balance
+    format('%.2f', @balance)
   end
 
+  def print_statement
+    # @deposit = deposit
+    # @show_balance = show_balance
+    p "date || credit || debit || balance \n date here || #{format('%.2f', @deposit_amount)} || #{format('%.2f', @withdrawal_amount)} || #{format('%.2f', @balance)}"
+  end
 end
-
-
-#
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -14,7 +14,7 @@ describe Account do
     it 'responds to deposit' do
       Account.new
       subject.deposit(10)
-      expect(subject.show_balance).to eq "10.00"
+      expect(subject.show_balance).to eq '10.00'
     end
   end
 
@@ -23,25 +23,26 @@ describe Account do
       Account.new
       subject.deposit(10)
       subject.withdraw(3)
-      expect(subject.show_balance).to eq "7.00"
+      expect(subject.show_balance).to eq '7.00'
     end
-   end
+  end
 
-# edge case: test for someone withdrawing more than they have
+  # edge case: test for someone withdrawing more than they have
 
   describe '#show_balance' do
     it 'should show current balance on account' do
       Account.new
       subject.deposit(10)
-      expect(subject.show_balance).to eq "10.00"
+      expect(subject.show_balance).to eq '10.00'
     end
   end
 
   describe '#print statement' do
-    it 'should initially print the value of a deposit in correct format' do
+    it 'should print the value of a deposit and witdrawal in correct format' do
       Account.new
       subject.deposit(10)
-      expect(subject.print_statement).to eq "date || credit || debit || balance\n || 10.00 || || 10.00"
+      subject.withdraw(3)
+      expect(subject.print_statement).to eq "date || credit || debit || balance \n date here || 10.00 || 3.00 || 7.00"
     end
   end
 end
